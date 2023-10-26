a960c04583758ee69a93596db5d691b8bfdfc1c1
SchlenkR <ronald@pure-state.de>
2023-10-26 08:34:20 +0000

    setup

---
+++ build/genDocs.ps1
@@ -0,0 +1,24 @@
+
+if ((Test-Path .\.fsdocs) -eq $true) {
+    Remove-Item .\.fsdocs\ -Force -Recurse
+}
+
+dotnet tool restore
+dotnet build ./src/FsHttp/FsHttp.fsproj -c Release -f netstandard2.0
+
+# what a hack...
+if ($null -eq $args[0]) {
+    $mode = "build"
+} else {
+    $mode = "watch"
+}
+
+dotnet fsdocs `
+    $mode `
+    --clean `
+    --sourcefolder ./src `
+    --input ./src/docs `
+    --output ./docs `
+    --properties Configuration=Release `
+    --sourcerepo https://github.com/fsprojects/FsHttp/blob/master/src `
+    --parameters root /FsHttp/ 
